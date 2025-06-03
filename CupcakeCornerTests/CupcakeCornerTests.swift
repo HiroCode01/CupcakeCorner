53aa6d017afec022f94c5fa4fc6aa26b9a6a69af
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-06-03 19:23:04 +0500

    Initial Commit

---
+++ CupcakeCornerTests/CupcakeCornerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerTests.swift
+//  CupcakeCornerTests
+//
+//  Created by Fazliddin Abdazimov on 03/06/25.
+//
+
+import Testing
+@testable import CupcakeCorner
+
+struct CupcakeCornerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
